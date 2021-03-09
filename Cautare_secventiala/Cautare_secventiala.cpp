458a234641822b7fbab861d1bcb66d8aa1e30b53
Beatrice Ciubotariu <betyciubotariu14@gmail.com>
2021-03-09 13:08:31 +0200

    Create Cautare_secventiala.cpp

---
+++ Cautare_secventiala/Cautare_secventiala.cpp
@@ -0,0 +1,30 @@
+// Cautare_secventiala.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, x, i, gasit, a[50];
+    cout << "Dati numarul de elemente ale tabloului:";
+    cin >> n;
+    cout << "Dati numarul de cautat:";
+    cin >> x;
+    cout << "Dati elementele tablului:"<<endl;
+    gasit = 0;
+    for (i = 0; i < n; i++)
+    {
+        cout << "a[" << i << "]=";
+        cin >> a[i];
+        if (a[i] == x)
+            gasit = 1;
+    }
+    if (gasit == 0)
+        cout << "Numarul cautat nu apartine tablului!" << endl;
+    else
+        cout << "Numarul apartine tablului!" << endl;
+    return 0;
+}
+
+
